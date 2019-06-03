231cf9124f40fd0d3dd0a1cd5df87fec5077f6ff
treyarnold <arnold.trey80@gmail.com>
2019-06-03 15:56:55 -0500

    initial folder structure

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
