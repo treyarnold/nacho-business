c9a6d153fe4b2edac41cf1fc1f8222bb3c2d7444
treyarnold <arnold.trey80@gmail.com>
2019-06-06 17:44:21 -0500

    connect syntax correction

--- config/connection.js
+++ config/connection.js
@@ -5,16 +5,16 @@ let connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {}
-  const connection = mysql.createConnection({
+} else {
+  connection = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
-});
+  });
+}
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
